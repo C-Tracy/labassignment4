b1cc4a9ca7993d99512c7419af7c2271d5efe448
Claire B. Tracy <claire5060@gmail.com>
2020-09-30 17:17:50 -0500

    Adding if else statement at beginning to print error message if less or more than one argument was input.

--- rev_comp.sh
+++ rev_comp.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+if [ $# -lt 1 ]; then echo "no file input, please include a fasta file with command"; fi
+if [ $# -gt 1 ]; then echo "more than one file was input, please provide only one fasta file"; fi
+
 #initialize variable
 reverse=""
 
